0ad703e911d63564a862d7cd9a40d87a68f44e07
DRodriguez08 <d.rodriguez.17@etec.uba.ar>
2022-09-13 16:53:36 -0300

    Test completados

--- database/script.sql
+++ database/script.sql
@@ -1,1 +1,11 @@
 -- Create here your sql script
+Create database biblioteca;
+use biblioteca;
+Create table autor (nombre varchar(255));
+insert into autor (nombre) values ("Jorge Luis Borges");
+insert into autor (nombre) values ("J. R. R. Tolkien");
+insert into autor (nombre) values ("Julio Verne");
+Create table libro (titulo varchar(255), autor varchar(255));
+insert into libro (titulo, autor) values ("El aleph", "Jorge Luis Borges");
+insert into libro (titulo, autor) values ("Rayuela", "Julio Cortázar");
+insert into libro (titulo, autor) values ("Alguien anda por ahí", "Julio Cortázar");
